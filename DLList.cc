24aa3b69a5ee9ee9123bc0b727023a46c2ddd801
JuanMa216 <juan.rojas6@utp.edu.co>
2025-03-04 23:17:18 -0500

    Implementacion de las listas doblemente enlazadas

--- DLList.cc
+++ DLList.cc
@@ -22,7 +22,88 @@ private:
             data_ = d;
             prev_ = next_ = nullptr;
         }
+
+        T getData() const {
+            return data_;
+        }
+
+        Node* getNext() const {
+            return next_;
+        }
+
+        Node* getPrev() const {
+            return prev_;
+        }
+
+        void setNext(Node *nxt) {
+            next_ = nxt;
+        }
+
+        void setPrev(Node *prv) {
+            prev_ = prv;
+        }
+
+        void setData(const T& data) {
+            data_ = data;
+        }
     }; 
 
+private:
+    Node *first_;
+    Node *last_;
+    unsigned int size_;
+
+public:
+    DLL() {
+        first_ = last_ = nullptr;
+        size_ = 0;
+    }
+
+    bool empty() const { return first_ == nullptr; }
+
+    unsigned int size() const { return size_; }
+
+    void print() {
+        Node* current = first_;
+        while (current != nullptr)
+        {
+            cout << current->getData() << " -> ";
+            current = current->getNext();
+        }
+    cout << "nullptr" << endl;
+    }
+
+    void push_front(const T& d) {
+        Node *n = new Node(d);
+
+        n->setNext(first_);
+
+        if (first_ != nullptr) {
+            first_->setPrev(n);  
+        }
+
+        first_ = n;
+        
+        if (size_ == 0)
+        {
+            last_ = n;
+        }
+        
+        size_++;
+    }
+
 };
+
+int main() {
+    cout << "Listas Dobles!" << endl;
+
+    DLL<int> myList;
+    myList.push_front(1);
+    myList.print();
+    myList.push_front(2);
+    myList.push_front(3);
+    myList.print();
+
+    return 0;
+}
     
