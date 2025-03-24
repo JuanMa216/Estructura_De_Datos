669a7c5c666565d6403ac1260e1574fe167bb86c
JuanMa216 <juan.rojas6@utp.edu.co>
2025-03-24 16:55:18 -0500

    Se agregó el metodo shrink_to_fit

--- Vectores.cc
+++ Vectores.cc
@@ -4,109 +4,133 @@
 using namespace std;
 
 template <typename T>
-class Vector {
-    private:
-    T* storage_;
+class Vector
+{
+private:
+    T *storage_;
     unsigned int capacity_;
     unsigned int size_;
-    public:
-    Vector(unsigned int c) {
+
+public:
+    Vector(unsigned int c)
+    {
         capacity_ = c;
         storage_ = new T[capacity_];
         size_ = 0;
     }
 
-    Vector() {
+    Vector()
+    {
         capacity_ = 5;
         storage_ = new T[capacity_];
         size_ = 0;
     }
 
-    Vector(unsigned int c, T elem) {
+    Vector(unsigned int c, T elem)
+    {
         capacity_ = c;
         storage_ = new T[capacity_];
-        for (unsigned int i = 0; i < capacity_; i++) {
+        for (unsigned int i = 0; i < capacity_; i++)
+        {
             storage_[i] = elem;
         }
-    size_ = capacity_;
+        size_ = capacity_;
     }
 
-    public:
-    unsigned int capacity() {
+public:
+    unsigned int capacity()
+    {
         return capacity_;
     }
 
-    unsigned int size() {
+    unsigned int size()
+    {
         return size_;
     }
 
-    T& at(unsigned int pos) {
+    T &at(unsigned int pos)
+    {
         assert(pos >= 0 && pos < size_);
         return storage_[pos];
     }
 
-    const T& at(unsigned int pos) const {
+    const T &at(unsigned int pos) const
+    {
         assert(pos >= 0 && pos < size_);
         return storage_[pos];
     }
 
-    const T& operator[](unsigned int index) const {
+    const T &operator[](unsigned int index) const
+    {
         return storage_[index];
     }
 
-    private:
-    void resize() {
+private:
+    void resize()
+    {
         cout << "Resize" << endl;
         unsigned int capacity2 = capacity_ * 1.5;
-        T* storage2 = new T[capacity2];
-        for ( unsigned int i = 0; i < size_; i++) {
+        T *storage2 = new T[capacity2];
+        for (unsigned int i = 0; i < size_; i++)
+        {
             storage2[i] = storage_[i];
         }
-        delete [] storage_;
+        delete[] storage_;
         storage_ = storage2;
         capacity_ = capacity2;
     }
 
-    public:
-    unsigned int waste() {
+public:
+    unsigned int waste()
+    {
         return capacity_ - size_;
     }
 
-    void push_front(const T& elem) {
+    void push_front(const T &elem)
+    {
         if (size_ == capacity_)
         {
             resize();
         }
 
-        for (unsigned int i = size_; i > 0; i--) {
+        for (unsigned int i = size_; i > 0; i--)
+        {
             swap(storage_[i], storage_[i - 1]);
         }
         storage_[0] = elem;
         size_++;
     }
 
-    void push_back(const T& elem) {
-        if (size_ == capacity_) {
+    void push_back(const T &elem)
+    {
+        if (size_ == capacity_)
+        {
             resize();
         }
         storage_[size_] = elem;
         size_++;
     }
 
-    void pop_back() {
-        if(size_ == 0) return;
+    void pop_back()
+    {
+        if (size_ == 0)
+            return;
         size_--;
     }
 
-    void pop_front() {
-        if(size_ == 0) return;
-        for (unsigned int i = 0; i < size_ - 1; i++) {
+    void pop_front()
+    {
+        if (size_ == 0)
+            return;
+        for (unsigned int i = 0; i < size_ - 1; i++)
+        {
             storage_[i] = storage_[i + 1];
         }
         size_--;
     }
 
-    void insert(unsigned int index, T element) {
+    void insert(unsigned int index, T element)
+    {
         if (index > size_)
         {
             throw std::out_of_range("Index out of bounds");
@@ -119,30 +143,44 @@ class Vector {
 
         for (unsigned int i = size_; i > size_; i--)
         {
-            storage_[i] = storage_[i-1];
+            storage_[i] = storage_[i - 1];
         }
-        
+
         storage_[index] = element;
         size_++;
     }
 
-    void print() {
-        for (unsigned int i = 0; i < size_; i++) {
+    void print()
+    {
+        for (unsigned int i = 0; i < size_; i++)
+        {
             cout << " " << storage_[i];
         }
         cout << endl;
     }
 
+    void shrink_to_fit()
+    {
+        T *storage2 = new T[size_];
+        for (unsigned int i = 0; i < size_; i++)
+        {
+            storage2[i] = storage_[i];
+        }
+        delete[] storage_;
+        storage_ = storage2;
+        capacity_ = size_;
+    }
 };
 
-int main() {
-    
+int main()
+{
+
     Vector<int> myVector(3);
     myVector.push_back(10);
     myVector.push_back(20);
     myVector.push_back(30);
 
-    cout << myVector.size() << endl; // Expected: 3
+    cout << myVector.size() << endl;     // Expected: 3
     cout << myVector.capacity() << endl; // Expected: Initial capacity, e.g., 4 or 8
 
     // Test pop_back
@@ -153,25 +191,24 @@ int main() {
     cout << myVector.at(0) << endl; // Expected: 10
     cout << myVector.at(1) << endl; // Expected: 20
     cout << myVector.capacity() << endl;
-    
+
     myVector.print();
-    myVector.insert(0,1);
+    myVector.insert(0, 1);
     myVector.print();
-    myVector.insert(1,2);
+    myVector.insert(1, 2);
     myVector.print();
-    myVector.insert(2,3);
-    myVector.print();
-    myVector.insert(6,4);
-    myVector.print();
-    //myVector.insert(3,4);
-    //myVector.print();
+    // myVector.insert(3,4);
+    // myVector.print();
 
     // Border case: Accessing an out-of-bounds index
-    //cout << myVector.at(2) << endl; // Should crash!
+    // cout << myVector.at(2) << endl; // Should crash!
 
     // Border case: pop_back on empty vector
-    //Vector<int> emptyVector;
-    //emptyVector.pop_back(); // should crash!
+    // Vector<int> emptyVector;
+    // emptyVector.pop_back(); // should crash!
+    myVector.shrink_to_fit();
+    cout << myVector.capacity() << endl;
+    myVector.print();
 
     return 0;
 }
